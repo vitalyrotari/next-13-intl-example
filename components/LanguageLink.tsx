6ac446b96693c8275f89fc6be29419cb4e7a20ce
vitalierotari <137866206+vitalierotari@users.noreply.github.com>
2023-09-14 14:15:33 +0300

    Initial commit

---
+++ components/LanguageLink.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import clsx from 'clsx';
+import Link from 'next-intl/link';
+import {usePathname} from 'next-intl/client';
+import {useTranslations, useLocale} from 'next-intl';
+import type {ComponentProps} from 'react';
+
+type LanguageLinkProps = Omit<ComponentProps<typeof Link>, 'href' | 'locale'> & {
+  locale: string;
+};
+
+export const LanguageLink = ({locale, ...rest}: LanguageLinkProps) => {
+  const currentLocale = useLocale();
+  const pathname = usePathname();
+  const isActive = locale === currentLocale
+
+  return (
+    <Link
+      aria-current={isActive ? 'page' : undefined}
+      className={clsx(
+        'inline-block px-2 py-3 transition-colors',
+        isActive ? 'text-black cursor-default' : 'text-blue-800 hover:text-blue-600 hover:underline'
+      )}
+      href={pathname}
+      locale={locale}
+      {...rest}
+    />
+  );
+}
---
+++ components/NavigationLink.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import clsx from 'clsx';
+import Link from 'next-intl/link';
+import {usePathname} from 'next-intl/client';
+import {useSelectedLayoutSegment} from 'next/navigation';
+import type {ComponentProps} from 'react';
+
+type NavigationLinkProps = Omit<ComponentProps<typeof Link>, 'href'> & {
+  href: string;
+};
+
+export const NavigationLink = ({href, ...rest}: NavigationLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      aria-current={isActive ? 'page' : undefined}
+      className={clsx(
+        'inline-block px-2 py-3 transition-colors',
+        isActive ? 'text-black cursor-default' : 'text-blue-800 hover:text-blue-600 hover:underline'
+      )}
+      href={href}
+      {...rest}
+    />
+  );
+}
